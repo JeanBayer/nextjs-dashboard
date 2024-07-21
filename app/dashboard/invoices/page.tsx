22e47f5b9b8672f503acafb83ca5256e2a3a8fa4
jmoraba_bci <jhan.mora@bci.cl>
2024-07-20 20:21:11 -0400

    chapter final

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -7,6 +7,12 @@ import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
 import { fetchInvoicesPages } from "@/app/lib/data";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Invoices",
+};
+
 export default async function Page({
   searchParams,
 }: {
@@ -18,7 +24,7 @@ export default async function Page({
   const query = searchParams?.query || "";
   const currentPage = Number(searchParams?.page) || 1;
   const totalPages = await fetchInvoicesPages(query);
-  
+
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
